6c84723411d3e39abb4d0cc1cc348475d541d0af
Tomson <tomsonj88@wp.pl>
2024-08-02 18:46:11 +0200

    Add file with agruments to argparse

---
+++ arg_parser.py
@@ -0,0 +1,45 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog="Pyendecryptor",
+                                 description="Program for encrypt/decrypt "
+                                             "message, file or directory"
+                                 )
+
+parser.add_argument("-m",
+                    "--mode",
+                    choices=["encrypt", "decrypt"],
+                    default="encrypt",
+                    help="Choose mode of operation: encrypt or decrypt"
+                    )
+data_group = parser.add_mutually_exclusive_group()
+data_group.add_argument("-f",
+                        "--file"
+                        )
+data_group.add_argument("-d",
+                        "--dir",
+                        help="Directory to encrypt/decrypt"
+                        )
+data_group.add_argument("-msg",
+                        "--message"
+                        )
+parser.add_argument("-p",
+                    "--password",
+                    help="Password for encryption/decryption"
+                    )
+parser.add_argument("-s",
+                    "--salt",
+                    help="Salt for password"
+                    )
+parser.add_argument("-ko",
+                    "--keep_originals",
+                    help="Remove original file/folder after "
+                         "encryption/decryption"
+                    )
+# parser.add_argument("-v",
+#                     "--verbose",
+#                     count=True,
+#                     default=0,
+#                     help="Verbose mode"
+#                     )
+
+args = parser.parse_args()
